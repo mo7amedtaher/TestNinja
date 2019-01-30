bab9d9278b2a45087b072f67cb5fcb6edcf69230
Taher <mo7amed_taher@hotmail.com>
2019-01-30 19:48:52 +0300

    MathTests

---
+++ NinjaTest.NUnitTest/ReservationTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+
+namespace NinjaTest.NUnitTest
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            //arrange
+            var Reservation = new Reservation();
+            //act
+            var result = Reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            //arrange
+            var user = new User { IsAdmin = false };
+            var Reservation = new Reservation { MadeBy = user };
+            Reservation.MadeBy = user;
+            //act
+            var result = Reservation.CanBeCancelledBy(user);
+            //assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherCancelling_ReturnsFalse()
+        {
+            //arrange
+            var Reservation = new Reservation { MadeBy = new User() };
+            //act
+            var result = Reservation.CanBeCancelledBy(new User { IsAdmin = false });
+            //assert
+            Assert.That(result == false);
+        }
+    }
+}
---
+++ TestNinja.UnitTest/MathTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+            Assert.AreEqual(result, 3);
+
+        }
+
+        [TestMethod]
+        public void Max_WhenFirstArgumentIsGreaterThanTheSecond_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+            var result = math.Max(2, 1);
+            Assert.AreEqual(result, 2);
+
+        }
+        [TestMethod]
+        public void Max_WhenSecondArgumentIsGreaterThanTheFirst_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+            var result = math.Max(1, 2);
+            Assert.AreEqual(result, 2);
+
+        }
+        [TestMethod]
+        public void Max_WhenTheTwoArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+            var result = math.Max(2, 2);
+            Assert.AreEqual(result, 2);
+
+        }
+    }
+}
